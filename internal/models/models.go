4dde9b4e19f3eb83706457080541c36f36b9b768
Aakash Pahwa <aakash10399@gmail.com>
2021-08-22 14:29:23 +0530

    Added models and interfaces

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,54 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Question struct {
+	QuestionID          uuid.UUID
+	TimeLimitMultiplier int
+	Testcases           []string
+}
+
+type ResponseModeType int
+
+const (
+	AfterEachTestcase ResponseModeType = 0
+	AfterAllTestcases ResponseModeType = 1
+)
+
+type CodeSubmission struct {
+	SourceCode   string
+	LanguageID   uuid.UUID
+	QuestionID   uuid.UUID
+	CallbackUrl  string
+	ResponseMode ResponseModeType
+	SubmissionID uuid.UUID
+}
+
+type GetResultsRequest struct {
+	SubmissionID uuid.UUID
+}
+
+type StandardResponse struct {
+	Success bool
+	Message string
+	Value   interface{}
+}
+
+type TestcaseVerdict string
+
+const (
+	CorrectAnswer       TestcaseVerdict = "AC"
+	WrongAnswer         TestcaseVerdict = "WA"
+	RuntimeError        TestcaseVerdict = "RE"
+	Processing          TestcaseVerdict = "PROC"
+	TimeLimitExceeded   TestcaseVerdict = "TLE"
+	MemoryLimitExceeded TestcaseVerdict = "MLE"
+	CompilationError    TestcaseVerdict = "CE"
+)
+
+type SubmissionResult struct {
+	SubmissionID uuid.UUID
+	Verdicts     map[string]TestcaseVerdict
+}
--- internal/services/services.go
+++ internal/services/services.go
@@ -1,1 +1,25 @@
 package services
+
+import (
+	"io"
+	"judgeinf/internal/models"
+
+	"github.com/google/uuid"
+)
+
+type Queue interface {
+	PushRequestToQueue(SubmissionID uuid.UUID)
+}
+
+type Table interface {
+	PushRequestMetadataToTable(CodeSubmission models.CodeSubmission)
+	FetchRequestMetadataFromTable(SubmissionID uuid.UUID) models.CodeSubmission
+	PushQuestionMetadataToTable(Question models.Question)
+	FetchQuestionMetadataToTable(QuestionID uuid.UUID) models.Question
+	PushResultsToTable(SubmissionResult models.SubmissionResult)
+	FetchResultsFromTable(SubmissionID uuid.UUID) models.SubmissionResult
+}
+
+type Storage interface {
+	PushTestcaseToStorage(Testcase io.Reader)
+}
